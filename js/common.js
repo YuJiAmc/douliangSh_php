89ae71c5bdf3e1eafbad6ec404faecd0ebc49618
YuJiAmc <1002273469@qq.com>
2021-02-01 16:11:17 +0800

    commit feat: Code formatter

--- js/common.js
+++ js/common.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function () {
   // $("#head").load("./header.html?v="+new Date().getTime());
   // $("#head_m").load("./header_m.html?v="+new Date().getTime());
   // $("#foot").load("./footer.html?v="+new Date().getTime());
@@ -6,31 +6,29 @@ $(function(){
   // $("#contact").load("./contact.html?v="+new Date().getTime());
   // $("#contact_m").load("./contact_m.html?v="+new Date().getTime());
   setTimeout(() => {
-      $(window).scroll(function () {
-          if ($(window).scrollTop() > $("#banner").height()){
-              $(".back_top").fadeIn(1000);
-          }
-          else
-          {
-              $(".back_top").fadeOut(1000);
-          }
-      })
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > $("#banner").height()) {
+        $(".back_top").fadeIn(1000);
+      } else {
+        $(".back_top").fadeOut(1000);
+      }
+    });
   }, 16.7);
   // 替换img
-  var test = /_min\./
-  $(".compress_img").each(function(){	
-    var reSrc = $(this).attr("src").replace(test,".");
-    $(this).attr("src",reSrc)
-  })
+  var test = /_min\./;
+  $(".compress_img").each(function () {
+    var reSrc = $(this).attr("src").replace(test, ".");
+    $(this).attr("src", reSrc);
+  });
   // 替换背景图
-  $(".compress_bgi").each(function(){
-    var img=new Image();
+  $(".compress_bgi").each(function () {
+    var img = new Image();
     var bgi = $(this).css("background-image");
-    var url = bgi.slice(5,-2).replace(test,".")
-    img.src= url;
+    var url = bgi.slice(5, -2).replace(test, ".");
+    img.src = url;
     var that = this;
-    img.onload=function(){
-      $(that).css("background-image","url("+url+")")
+    img.onload = function () {
+      $(that).css("background-image", "url(" + url + ")");
     };
-  })
+  });
 });
